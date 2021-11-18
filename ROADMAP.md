6a349278bb553d246721f2fffe2aede0cfe01e8a
Wu Wencheng <78869499+WuWencheng21@users.noreply.github.com>
2021-11-18 18:41:23 +0000

    Implementazione metodo reload (incompleto)

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,16 +1,16 @@
 # ROADMAP
 
   - [v] Accedere al web service leggendo i dati
-  - [v visualizzazione semplice (in console) dei dati ricevuti
+  - [v] visualizzazione semplice (in console) dei dati ricevuti
   - [v] visualizzazione nella pagina HTML dei dati
   - [ ] modifica dei dati per i sensori che lo consentono
   - [v] per i sensori con dati continui (readonly = true) aggiornamento dei dati
     - [v] premendo un bottone
-    - [] utilizzo un timer e l'aggiornamento avviene in automatico
+    - [v] utilizzo un timer e l'aggiornamento avviene in automatico
   - [v] salvataggio dello storico dei dati 
-    -[] tutti i dati
+    -[v] tutti i dati
     -[v] ultimi 10 dati
   - [v] visualizzazione dello storico in una tabella  
-  - [] visualizzazione con un grafico (https://www.chartjs.org/docs/latest/ ma libertà  di scelta)
+  - [v] visualizzazione con un grafico (https://www.chartjs.org/docs/latest/ ma libertà  di scelta)
   - [] layout HTML e grafica (uikit, .... libertà di scelta)
    
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const debug = document.getElementById("debug")
+//const value = document.getElementById("value")
 const type = document.getElementById("type")
 var typeSensor1 = document.getElementById("typeSensor1")
 var typeSensor2 = document.getElementById("typeSensor2")
@@ -21,12 +22,14 @@ fetch("https://hf3xzw.deta.dev/")
   .then(r => r.json())
   .then(body => {
     console.log(body)
+    //let value = ""
     let type = ""
     let read = ""
     for (i = 0; i < body.sensors.length; i++) {
       read = JSON.stringify(body.sensors[i].readonly, null, 2)
       //if(let == "true"){
-      type = type + JSON.stringify(body.sensors[i].description, null, 2)
+      //value = value + JSON.stringify(body.sensors[i].value, null, 2) +"<br>"
+      type = type + JSON.stringify(body.sensors[i].description, null, 2) +"<br>"
       //}
       if (i >= 4) {
         storico[j] = "<br>" + JSON.stringify(body.sensors[i].value, null, 2)
@@ -43,12 +46,13 @@ fetch("https://hf3xzw.deta.dev/")
     }
     document.getElementById("debug").innerHTML = JSON.stringify(body, null, 2)
     document.getElementById("type").innerHTML = type
+    //document.getElementById("value").innerHTML = value
     document.getElementById("Storico").innerHTML = storico
     document.getElementById("typeSensor1").innerHTML = typeSensor1
     document.getElementById("typeSensor2").innerHTML = typeSensor2
     document.getElementById("typeSensor3").innerHTML = typeSensor3
     document.getElementById("typeSensor4").innerHTML = typeSensor4
-
+    //grafico
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
       type: 'bar',
@@ -91,23 +95,15 @@ fetch("https://hf3xzw.deta.dev/")
 
 const btnAggiorna = document.getElementById("btnAggiorna")
 btnAggiorna.onclick = () => {
-  //fetch("https://python-iot-sim.professorandrea.repl.co")
-  //document.body.innerHTML = ""
-  //interval = setTimeout(function() { reload(); }, 1000);
-  //clearInterval(interval)
-//}
-
-
-/*function reload () {*/
   fetch("https://hf3xzw.deta.dev/")
     .then(r => r.json())
     .then(body => {
       console.log(body)
+      //let value = ""
       let read = ""
       for (i = 0; i < body.sensors.length; i++) {
         read = JSON.stringify(body.sensors[i].readonly, null, 2)
-        //if(let == "true"){
-        //value = value + JSON.stringify(body.sensors[i].value,null,2) + "<br>" 
+        //value = value + JSON.stringify(body.sensors[i].value, null, 2) + "<br>"
         if (i >= 4) {
           if (j == 8) {
             for (m = 0; m < 4; m++) {
@@ -122,8 +118,6 @@ btnAggiorna.onclick = () => {
         typeSensor2Value = JSON.stringify(body.sensors[5].value, null, 2)
         typeSensor3Value = JSON.stringify(body.sensors[6].value, null, 2)
         typeSensor4Value = JSON.stringify(body.sensors[7].value, null, 2)
-
-        //}
       }
       document.getElementById("debug").innerHTML = JSON.stringify(body, null, 2)
       //document.getElementById("value").innerHTML = value
@@ -132,8 +126,76 @@ btnAggiorna.onclick = () => {
       document.getElementById("typeSensor2").innerHTML = typeSensor2
       document.getElementById("typeSensor3").innerHTML = typeSensor3
       document.getElementById("typeSensor4").innerHTML = typeSensor4
+      const ctx = document.getElementById('myChart').getContext('2d');
+      const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: [typeSensor1, typeSensor2, typeSensor3, typeSensor4],
+          datasets: [{
+            label: 'On/Off',
+            data: [typeSensor1Value, typeSensor2Value, typeSensor3Value, typeSensor4Value],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    })
+}
 
-
+function reload() {
+  fetch("https://hf3xzw.deta.dev/")
+    .then(r => r.json())
+    .then(body => {
+      console.log(body)
+      let type = ""
+      let read = ""
+      for (i = 0; i < body.sensors.length; i++) {
+        read = JSON.stringify(body.sensors[i].readonly, null, 2)
+        //if(let == "true"){
+        type = type + JSON.stringify(body.sensors[i].description, null, 2)
+        //}
+        if (i >= 4) {
+          storico[j] = "<br>" + JSON.stringify(body.sensors[i].value, null, 2)
+          j++
+          typeSensor1 = JSON.stringify(body.sensors[4].description, null, 2)
+          typeSensor2 = JSON.stringify(body.sensors[5].description, null, 2)
+          typeSensor3 = JSON.stringify(body.sensors[6].description, null, 2)
+          typeSensor4 = JSON.stringify(body.sensors[7].description, null, 2)
+          typeSensor1Value = JSON.stringify(body.sensors[4].value, null, 2)
+          typeSensor2Value = JSON.stringify(body.sensors[5].value, null, 2)
+          typeSensor3Value = JSON.stringify(body.sensors[6].value, null, 2)
+          typeSensor4Value = JSON.stringify(body.sensors[7].value, null, 2)
+        }
+      }
+      document.getElementById("debug").innerHTML = JSON.stringify(body, null, 2)
+      document.getElementById("type").innerHTML = type
+      document.getElementById("Storico").innerHTML = storico
+      document.getElementById("typeSensor1").innerHTML = typeSensor1
+      document.getElementById("typeSensor2").innerHTML = typeSensor2
+      document.getElementById("typeSensor3").innerHTML = typeSensor3
+      document.getElementById("typeSensor4").innerHTML = typeSensor4
       const ctx = document.getElementById('myChart').getContext('2d');
       const myChart = new Chart(ctx, {
         type: 'bar',
@@ -169,82 +231,86 @@ btnAggiorna.onclick = () => {
           }
         }
       });
-
-
     })
 }
 
-/*function reload(){
-  fetch("https://hf3xzw.deta.dev/")
-  .then(r => r.json())
-  .then(body => {
-    console.log(body)
-    let type = ""
-    let read = ""
-    for (i = 0; i < body.sensors.length; i++) {
-      read = JSON.stringify(body.sensors[i].readonly, null, 2)
-      //if(let == "true"){
-      type = type + JSON.stringify(body.sensors[i].description, null, 2)
-      //}
-      if (i >= 4) {
-        storico[j] = "<br>" + JSON.stringify(body.sensors[i].value, null, 2)
-        j++
-        typeSensor1 = JSON.stringify(body.sensors[4].description, null, 2)
-        typeSensor2 = JSON.stringify(body.sensors[5].description, null, 2)
-        typeSensor3 = JSON.stringify(body.sensors[6].description, null, 2)
-        typeSensor4 = JSON.stringify(body.sensors[7].description, null, 2)
-        typeSensor1Value = JSON.stringify(body.sensors[4].value, null, 2)
-        typeSensor2Value = JSON.stringify(body.sensors[5].value, null, 2)
-        typeSensor3Value = JSON.stringify(body.sensors[6].value, null, 2)
-        typeSensor4Value = JSON.stringify(body.sensors[7].value, null, 2)
-      }
-    }
-    document.getElementById("debug").innerHTML = JSON.stringify(body, null, 2)
-    document.getElementById("type").innerHTML = type
-    document.getElementById("Storico").innerHTML = storico
-    document.getElementById("typeSensor1").innerHTML = typeSensor1
-    document.getElementById("typeSensor2").innerHTML = typeSensor2
-    document.getElementById("typeSensor3").innerHTML = typeSensor3
-    document.getElementById("typeSensor4").innerHTML = typeSensor4
 
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: [typeSensor1, typeSensor2, typeSensor3, typeSensor4],
-        datasets: [{
-          label: '',
-          data: [typeSensor1Value, typeSensor2Value, typeSensor3Value, typeSensor4Value],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
+//Aggiornamento dei dati con un timer
+/*btnAggiorna.onclick = () => {
+  console.log("click")
+  btnAggiorna.disabled = true
+  setInterval(() => {
+    fetch("https://hf3xzw.deta.dev/")
+      .then(r => r.json())
+      .then(body => {
+        console.log(body)
+        let read = ""
+        for (i = 0; i < body.sensors.length; i++) {
+          read = JSON.stringify(body.sensors[i].readonly, null, 2)
+          //if(let == "true"){
+          //value = value + JSON.stringify(body.sensors[i].value,null,2) + "<br>" 
+          if (i >= 4) {
+            if (j == 8) {
+              for (m = 0; m < 4; m++) {
+                storico[m] = storico[m + 4]
+              }
+              j = 4
+            }
+            storico[j] = "<br>" + JSON.stringify(body.sensors[i].value, null, 2)
+            j++
           }
+          typeSensor1Value = JSON.stringify(body.sensors[4].value, null, 2)
+          typeSensor2Value = JSON.stringify(body.sensors[5].value, null, 2)
+          typeSensor3Value = JSON.stringify(body.sensors[6].value, null, 2)
+          typeSensor4Value = JSON.stringify(body.sensors[7].value, null, 2)
         }
-      }
-    });
-  })
-}*/
+        document.getElementById("debug").innerHTML = JSON.stringify(body, null, 2)
+        //document.getElementById("value").innerHTML = value
+        document.getElementById("Storico").innerHTML = storico
+        document.getElementById("typeSensor1").innerHTML = typeSensor1
+        document.getElementById("typeSensor2").innerHTML = typeSensor2
+        document.getElementById("typeSensor3").innerHTML = typeSensor3
+        document.getElementById("typeSensor4").innerHTML = typeSensor4
 
+
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: [typeSensor1, typeSensor2, typeSensor3, typeSensor4],
+            datasets: [{
+              label: '',
+              data: [typeSensor1Value, typeSensor2Value, typeSensor3Value, typeSensor4Value],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+      })
+  }, 1000)
+}*/
 
 function modifica() {
   let ID = parseInt(document.getElementById('ID').value)
